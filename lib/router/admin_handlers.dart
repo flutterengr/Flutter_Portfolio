031f7aa687e25b47ea09396280db96392183f763
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2022-06-14 21:59:10 -0300

    Validacion De Usuarios y Navegacion

--- lib/router/admin_handlers.dart
+++ lib/router/admin_handlers.dart
@@ -1,5 +1,8 @@
 import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
+import 'package:twitter_project/providers/auth_provider.dart';
 
+import '../ui/views/dashboard_view.dart';
 import '../ui/views/login_view.dart';
 import '../ui/views/register_view.dart';
 
@@ -7,16 +10,32 @@ class AdminHandlers {
 
   static Handler login = Handler(
     handlerFunc: ( context, params ) {
-      return const LoginView();
+
+      final authProvider = Provider.of<AuthProvider>(context!);
+
+      if ( authProvider.authStatus == AuthStatus.notAuthenticated ) {
+        return LoginView();
+      } else {
+        return const DashboardView();
+      }
+
     }
   );
 
   static Handler register = Handler(
     handlerFunc: ( context, params ) {
-      return const RegisterView();
+      
+      final authProvider = Provider.of<AuthProvider>(context!);
+      
+      if ( authProvider.authStatus == AuthStatus.notAuthenticated ) {
+        return const RegisterView();
+      } else {
+        return const DashboardView();
+      }
     }
   );
 
 
 }
+
 
--- lib/ui/views/register_view.dart
+++ lib/ui/views/register_view.dart
@@ -1,5 +1,7 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import '../../providers/register_form_provider.dart';
 import '../../router/router.dart';
 import '../buttons/custom_outlined_button.dart';
 import '../buttons/link_text copy.dart';
@@ -11,74 +13,103 @@ class RegisterView extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 100),
-      padding: const EdgeInsets.symmetric( horizontal: 20 ),
-      child: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints( maxWidth: 370 ),
-          child: Form(
-            child: Column(
-              children: [
+    return ChangeNotifierProvider(
+      create: ( _ ) => RegisterFormProvider(),
+      child: Builder(builder: (context) {
 
-                // Email
-                TextFormField(
-                  // validator: (),
-                  style: const TextStyle( color: Colors.white ),
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: 'Ingrese su nombre',
-                    label: 'Nombre',
-                    icon: Icons.supervised_user_circle_sharp
-                  ),
-                ),
+        final registerFormProvider = Provider.of<RegisterFormProvider>(context, listen: false);
 
-                const SizedBox( height: 20 ),
-                
-                // Email
-                TextFormField(
-                  // validator: (),
-                  style: const TextStyle( color: Colors.white ),
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: 'Ingrese su correo',
-                    label: 'Email',
-                    icon: Icons.email_outlined
-                  ),
-                ),
+        return Container(
+          margin: const EdgeInsets.only(top: 50),
+          padding: const EdgeInsets.symmetric( horizontal: 20 ),
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints( maxWidth: 370 ),
+              child: Form(
+                autovalidateMode: AutovalidateMode.always,
+                key: registerFormProvider.formKey,
+                child: Column(
+                  children: [
 
-                const SizedBox( height: 20 ),
+                    // Email
+                    TextFormField(
+                      onChanged: ( value ) => registerFormProvider.name = value,
+                      validator: ( value ) {
+                        if ( value == null || value.isEmpty ) return 'El nombre es obligatario';
+                        return null;
+                      },
+                      style: const TextStyle( color: Colors.white ),
+                      decoration: CustomInputs.loginInputDecoration(
+                        hint: 'Ingrese su nombre',
+                        label: 'Nombre',
+                        icon: Icons.supervised_user_circle_sharp
+                      ),
+                    ),
 
-                // Password
-                TextFormField(
-                  // validator: (),
-                  obscureText: true,
-                  style: const TextStyle( color: Colors.white ),
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: '*********',
-                    label: 'Contraseña',
-                    icon: Icons.lock_outline_rounded
-                  ),
-                ),
-                
-                const SizedBox( height: 20 ),
-                CustomOutlinedButton(
-                  onPressed: () {}, 
-                  text: 'Crear cuenta',
-                ),
+                    const SizedBox( height: 20 ),
+                    
+                    // Email
+                    TextFormField(
+                      onChanged: ( value ) => registerFormProvider.email = value,
+                      validator: ( value ) {
+                        if( !EmailValidator.validate(value ?? '') ) return 'Email no válido';
+                        return null;
+                      },
+                      style: const TextStyle( color: Colors.white ),
+                      decoration: CustomInputs.loginInputDecoration(
+                        hint: 'Ingrese su correo',
+                        label: 'Email',
+                        icon: Icons.email_outlined
+                      ),
+                    ),
+
+                    const SizedBox( height: 20 ),
+
+                    // Password
+                    TextFormField(
+                      onChanged: ( value ) => registerFormProvider.password = value,
+                      validator: ( value ) {
+                        if ( value == null || value.isEmpty ) return 'Ingrese su contraseña';
+                        if ( value.length < 6 ) return 'La contraseña debe de ser de 6 caracteres';
+
+                        return null; // Válido
+                      },
+                      obscureText: true,
+                      style: TextStyle( color: Colors.white ),
+                      decoration: CustomInputs.loginInputDecoration(
+                        hint: '*********',
+                        label: 'Contraseña',
+                        icon: Icons.lock_outline_rounded
+                      ),
+                    ),
+                    
+                    const SizedBox( height: 20 ),
+                    CustomOutlinedButton(
+                      onPressed: () {
+
+                        registerFormProvider.validateForm();
+
+                      }, 
+                      text: 'Crear cuenta',
+                    ),
 
 
-                const SizedBox( height: 20 ),
-                LinkText(
-                  text: 'Ir al login',
-                  onPressed: () {
-                    Navigator.pushNamed(context, Flurorouter.loginRoute );
-                  },
+                    const SizedBox( height: 20 ),
+                    LinkText(
+                      text: 'Ir al login',
+                      onPressed: () {
+                        Navigator.pushNamed(context, Flurorouter.loginRoute );
+                      },
+                    )
+
+                  ],
                 )
-
-              ],
-            )
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+
+      }),
     );
   }
 
